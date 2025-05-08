3226eb0adb04dad4513f3c0f124e86d2e8971829
Atish <atishgurung13@gmail.com>
2025-05-08 12:45:45 +1000

    Implemented role-based access control

--- jukebox-backend/routes/authRoutes.js
+++ jukebox-backend/routes/authRoutes.js
@@ -1,20 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controller/authController');
-const { spotifyLogin, spotifyCallback } = require('../controller/spotifyController');
+const { getUser, updateUser, deleteUser} = require('../controller/userController')
+const authenticate = require('../middleware/authMiddleware')
 
 //GET
-router.get('/spotify', spotifyLogin);
-router.get('/spotify/callback', spotifyCallback);
+router.get('/getUserProfile', authenticate, getUser)
 
 //POST
 router.post('/register', register);
 router.post('/login', login);
 
 //PUT
-
+router.put('/profileUpdate', authenticate, updateUser);
 
 //DELETE
+router.delete('/deteleProfile', authenticate, deleteUser);
 
 
 
--- jukebox-backend/server.js
+++ jukebox-backend/server.js
@@ -6,13 +6,14 @@ const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
 // Routes
 const authRoutes = require('./routes/authRoutes');
-const chatRoutes = require('./routes/chatRoutes');
 
 app.use(express.json()); // Tells the app to parse incoming requests with JSON body
 app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from 'public' folder
 app.use(express.urlencoded({ extended: false }));
+app.use(cors()); // allow all origins during development
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -28,7 +29,6 @@ mongoose.connection.on('error', (err) => {
 });
 
 app.use('/api/auth', authRoutes);
-app.use('/api/chat', chatRoutes);
 
 // Start the server and listen on the specified port
 app.listen(PORT, () => {
