3def7c3a4363bb9527c0416972dc380b01b38345
Atish <atishgurung13@gmail.com>
2025-05-08 13:36:42 +1000

    Added spotify integration

--- jukebox-backend/routes/authRoutes.js
+++ jukebox-backend/routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { register, login } = require('../controller/authController');
-const { getUser, updateUser, deleteUser} = require('../controller/userController')
-const authenticate = require('../middleware/authMiddleware')
+const { getUser, updateUser, deleteUser} = require('../controller/userController');
+const authenticate = require('../middleware/authMiddleware');
+const { spotifyLogin, spotifyCallback } = require('../controller/spotifyController');
 
 //GET
-router.get('/getUserProfile', authenticate, getUser)
+router.get('/getUserProfile', authenticate, getUser);
+router.get('/spotify', spotifyLogin);
+router.get('/spotify/callback', spotifyCallback);
 
 //POST
 router.post('/register', register);
