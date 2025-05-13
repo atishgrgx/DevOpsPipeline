5d3a18fc047812d2ace944d9c9003d757856dbf2
MenuraDewalegama <menuradewalegama@gmail.com>
2025-05-14 00:16:31 +1000

    adding save song by name function to the music management component close #17

--- jukebox-backend/routes/songRoutes.js
+++ jukebox-backend/routes/songRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { saveSongsFromFile, saveSongsByName } = require('../controller/songController.js');
 
 router.get('/save-from-file', saveSongsFromFile);
-router.get('/search/:trackName', saveSongsByName);
+router.get('/search/:songName', saveSongsByName);
+
 
 module.exports = router;
