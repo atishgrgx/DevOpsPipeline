2ead0be6ef96ddbb0411289e34331683e0e89056
MenuraDewalegama <menuradewalegama@gmail.com>
2025-05-14 00:46:56 +1000

    adding a route to get all songs

--- jukebox-backend/routes/songRoutes.js
+++ jukebox-backend/routes/songRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { saveSongsFromFile, saveSongsByName, deleteSongById } = require('../controller/songController.js');
+const { saveSongsFromFile, saveSongsByName, deleteSongById, getAllSongs } = require('../controller/songController.js');
 
 router.get('/save-from-file', saveSongsFromFile);
 router.get('/search/:songName', saveSongsByName);
 router.delete('/:songId', deleteSongById);
+router.get('/songs', getAllSongs); 
 
 module.exports = router;
