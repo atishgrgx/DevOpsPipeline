21cb389cf771c7de2e55e872e68ef4fcfae56da7
Varniah88 <jaanuvarni@gmail.com>
2025-05-22 22:11:58 +1000

    artist section api and frontend integration

--- jukebox-backend/routes/songRoutes.js
+++ jukebox-backend/routes/songRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { saveSongsFromFile, saveSongsByName, deleteSongById, getAllSongs, getSongByIdDB,getTopSongs } = require('../controller/songController.js');
+const { saveSongsFromFile, saveSongsByName, deleteSongById, getAllSongs, getSongByIdDB,getTopSongs,getTopArtists } = require('../controller/songController.js');
 
 router.get('/save-from-file', saveSongsFromFile);
 router.get('/search/:songName', saveSongsByName);
 router.get('/top-songs', getTopSongs);
+router.get('/top-artists', getTopArtists);
 router.delete('/remove/:songId', deleteSongById);
 router.get('/', getAllSongs); 
 router.get('/:id', getSongByIdDB); 
