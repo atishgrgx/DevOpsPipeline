7a0b72f1cd6ac6c0c2e526345c50d8c55907ce6b
MenuraDewalegama <menuradewalegama@gmail.com>
2025-05-26 02:36:13 +1000

    adding e2e tests

--- jukebox-frontend/cypress/e2e/user_profile.cy.js
+++ jukebox-frontend/cypress/e2e/user_profile.cy.js
@@ -1,62 +1,76 @@
 describe('Music Management Test', () => {
 
+    let user_data;
+
     beforeEach(() => {
+        cy.fixture('credentials').then((data) => {
+            user_data = data;
+        });
         cy.visit('/')
     })
 
+    // Login as a user to the jukebox
     it('Login as user', () => {
+        // Navigate to the login page
         cy.get('[class="welcome"]').should('contain.text', 'WELCOME TO JUKEBOX')
         cy.get('[class="btn-large start"]').should('contain.text', 'CLICK HERE')
         cy.get('[class="btn-large start"]').should('contain.text', 'CLICK HERE').click()
         cy.url().should('include', 'login')
 
-        cy.get('[id="email"]').type('e2e@email.com')
-        cy.get('[id="password"]').type('e2etest')
+        // Enter user credentials
+        cy.get('[id="email"]').type(user_data.user.email)
+        cy.get('[id="password"]').type(user_data.user.password)
         cy.get('[type="submit"]').should('contain.text', 'Login').click()
         cy.contains(/Login Successful!/i).should('be.visible')
+
+        // Navigate to the homepage
         cy.url().should('include', 'home')
         cy.contains(/Want to Discover New Favorites Based on Your Taste?/i).should('be.visible')
     })
 
+    // User profile navigation end to end test
     it('Navigate to the profile page', () => {
+        // Navigate to the login page
         cy.get('[class="welcome"]').should('contain.text', 'WELCOME TO JUKEBOX')
         cy.get('[class="btn-large start"]').should('contain.text', 'CLICK HERE')
         cy.get('[class="btn-large start"]').should('contain.text', 'CLICK HERE').click()
         cy.url().should('include', 'login')
 
+        // Enter user credentials
         cy.get('[id="email"]').type('e2e@email.com')
         cy.get('[id="password"]').type('e2etest')
         cy.get('[type="submit"]').should('contain.text', 'Login').click()
         cy.contains(/Login Successful!/i).should('be.visible')
+
+        // Navigate to the homepage
         cy.url().should('include', 'home')
         cy.contains(/Want to Discover New Favorites Based on Your Taste?/i).should('be.visible')
 
+        // Navigate to the user profile page
         cy.get('[class="chat-btn btn-floating btn-large pink darken-1"]').click()
         cy.url().should('include', 'profile')
     })
 
-    it.only('Start a group chat', () => {
-        cy.get('[class="welcome"]').should('contain.text', 'WELCOME TO JUKEBOX')
-        cy.get('[class="btn-large start"]').should('contain.text', 'CLICK HERE')
-        cy.get('[class="btn-large start"]').should('contain.text', 'CLICK HERE').click()
-        cy.url().should('include', 'login')
+    // it.only('Start a group chat', () => {
+    //     cy.get('[class="welcome"]').should('contain.text', 'WELCOME TO JUKEBOX')
+    //     cy.get('[class="btn-large start"]').should('contain.text', 'CLICK HERE')
+    //     cy.get('[class="btn-large start"]').should('contain.text', 'CLICK HERE').click()
+    //     cy.url().should('include', 'login')
 
-        cy.get('[id="email"]').type('e2e@email.com')
-        cy.get('[id="password"]').type('e2etest')
-        cy.get('[type="submit"]').should('contain.text', 'Login').click()
-        cy.contains(/Login Successful!/i).should('be.visible')
-        cy.url().should('include', 'home')
-        cy.contains(/Want to Discover New Favorites Based on Your Taste?/i).should('be.visible')
+    //     cy.get('[id="email"]').type('e2e@email.com')
+    //     cy.get('[id="password"]').type('e2etest')
+    //     cy.get('[type="submit"]').should('contain.text', 'Login').click()
+    //     cy.contains(/Login Successful!/i).should('be.visible')
+    //     cy.url().should('include', 'home')
+    //     cy.contains(/Want to Discover New Favorites Based on Your Taste?/i).should('be.visible')
 
-        cy.get('[class="chat-btn btn-floating btn-large pink darken-1"]').click()
-        cy.url().should('include', 'profile')
+    //     cy.get('[class="chat-btn btn-floating btn-large pink darken-1"]').click()
+    //     cy.url().should('include', 'profile')
 
-        cy.get('[id="groupChat"]').click()
-        cy.contains(/Join a Genre Room/i).should('be.visible')
-        // cy.get('[id="genreSelect"]').click()
-        cy.get('.div.test-select-genre').select(1).should('have.value', 'pop')
-
-
-    })
+    //     cy.get('[id="groupChat"]').click()
+    //     cy.contains(/Join a Genre Room/i).should('be.visible')
+    //     // cy.get('[id="genreSelect"]').click()
+    //     cy.get('.div.test-select-genre').select(1).should('have.value', 'pop')
+    // })
 
 })
